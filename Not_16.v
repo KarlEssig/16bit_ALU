9084cd711ede999b0f91043fbc2a799f4c0ad088
umaymahsultana <48414999+umaymahsultana@users.noreply.github.com>
2019-04-17 21:19:32 -0500

    Update and rename not_16.v to Not_16.v

--- Not_16.v
+++ Not_16.v
@@ -1,3 +1,4 @@
+
 module Not_16(input [15:0] a, output [15:0] s);
 	assign s[0] = ~a[0];
 	assign s[1] = ~a[1];
@@ -15,4 +16,27 @@ module Not_16(input [15:0] a, output [15:0] s);
 	assign s[13] = ~a[13];
 	assign s[14] = ~a[14];
 	assign s[15] = ~a[15];
+endmodule
+
+module testbench();
+
+//Testing Not_16
+
+  reg [15:0] a;
+  wire [15:0] s;
+
+  Not_16 simpleNot(a, s);
+
+  initial begin 
+    a = 0;
+    #250  a=16'b0000010001110101;
+    #250  a=16'b0111010001110101;
+    #250  a=16'b1111010001110101;
+  end
+
+  initial begin
+    $display("        A                          NOT    ");
+    $monitor(" %b          %b         ", a, s);
+  end
+
 endmodule
