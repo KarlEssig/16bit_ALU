16200e1c9446c3b80f16d6cff42f9672414611bd
KarlEssig <48457439+KarlEssig@users.noreply.github.com>
2019-04-18 21:48:22 -0500

    Update DFlip.v

--- DFlip.v
+++ DFlip.v
@@ -1,4 +1,4 @@
-module DFlipFlop2(input [1:0] a, clock, reset, output reg [1:0] s);
+/*module DFlipFlop2(input [1:0] a, clock, reset, output reg [1:0] s);
 	always @(posedge clock)
 	begin
 		s = a;
@@ -8,6 +8,29 @@ module DFlipFlop2(input [1:0] a, clock, reset, output reg [1:0] s);
 		s = 0;
 	end
 endmodule
+*/
+module DFlipFlopRUNNING(input a, clock, reset, output reg s);
+	always @(posedge clock)
+	begin
+		s = 1;
+	end
+	always @(posedge reset)
+	begin
+		s = 1;
+	end
+endmodule
+
+module DFlipFlopERROR(input a, clock, reset, output reg s);
+	always @(posedge clock)
+	begin
+		s = a;
+	end
+	always @(posedge reset)
+	begin
+		s = 0;
+	end
+endmodule
+
 
 module DFlipFlop16(input [15:0] a, clock, reset, output reg [15:0] s);
 	always @(posedge clock)
